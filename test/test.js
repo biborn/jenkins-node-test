d776b3f03c435243a3a4395950cae15e3c9108ee
Rashid Razak <bib.nexus@gmail.com>
2018-11-01 03:51:31 +0800

    Fix test not exiting due to open connection to MongoDB

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 const request = require('supertest');
 const assert = require('assert');
 const server = require('../index');
+const mongoose = require('mongoose');
 
 describe('GET /', function() {
     it('displays hello', function(done) {
-        request(server).get('/').expect('Hello!', done);
+        request(server).get('/').expect('Hello!', function() {
+            server.close(function() {
+                mongoose.connection.close(done);
+            });
+        });
     });
 });
